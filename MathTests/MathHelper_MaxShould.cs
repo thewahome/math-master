e22d6db2216a0fbf860cc2e48404efb0d29107fc
Charles Wahome <thewahome.cw@gmail.com>
2020-01-10 15:20:09 +0300

    tests if numbers are the same

--- MathTests/MathHelper_MaxShould.cs
+++ MathTests/MathHelper_MaxShould.cs
@@ -19,6 +19,16 @@ namespace MathTests
             Assert.Equal(expectedValue, result);
         }
 
-
+        [Theory]
+        [InlineDataAttribute(89,89,89)]
+        [InlineDataAttribute(-40789,-40789,-40789)]
+        [InlineDataAttribute(275,275,275)]
+        [InlineDataAttribute(0,0,0)]
+        public void ReturnValueWhenValueOfOperandsIsTheSame(int value1, int value2, int expectedResult)
+        {
+            var expectedValue = expectedResult;
+            var result = _mathHelper.Max(value1,value2);
+            Assert.Equal(expectedValue, result);
+        }
     }
 }
